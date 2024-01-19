ff83529ce16e9fb92905a99aa1bc9036ea02ae5f
Ariful145 <arif.ahmeed145@gmail.com>
2024-01-19 18:33:00 +0600

    added jwt method

--- src/Auth/Authprovider/AuthProvider.jsx
+++ src/Auth/Authprovider/AuthProvider.jsx
@@ -1,53 +1,54 @@
 import {
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    getAuth,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    signOut,
-    updateProfile,
-  } from "firebase/auth";
-  
-  import { createContext, useEffect, useState } from "react";
-  import PropTypes from "prop-types";
+   GoogleAuthProvider,
+   createUserWithEmailAndPassword,
+   getAuth,
+   onAuthStateChanged,
+   signInWithEmailAndPassword,
+   signInWithPopup,
+   signOut,
+   updateProfile,
+} from "firebase/auth";
+
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import app from "../Firebase/firebase.config";
-  
-  export const Context = createContext("");
-  
-  const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState("");
-    const [loader, setLoader] = useState(true);
-    const auth = getAuth(app);
-  
-    const googleProvider = new GoogleAuthProvider();
-  
-    const signWithGoogle = () => {
+import axios from "axios";
+
+export const Context = createContext("");
+
+const AuthProvider = ({ children }) => {
+   const [user, setUser] = useState("");
+   const [loader, setLoader] = useState(true);
+   const auth = getAuth(app);
+
+   const googleProvider = new GoogleAuthProvider();
+
+   const signWithGoogle = () => {
       setLoader(true);
       return signInWithPopup(auth, googleProvider);
-    };
-  
-    const signUpUser = (email, password) => {
+   };
+
+   const signUpUser = (email, password) => {
       setLoader(true);
       return createUserWithEmailAndPassword(auth, email, password);
-    };
-  
-    const updateUserProfile = (name, photoUrl) => {
+   };
+
+   const updateUserProfile = (name, photoUrl) => {
       setLoader(true);
       return updateProfile(auth.currentUser, {
-        displayName: name,
-        photoURL: photoUrl,
+         displayName: name,
+         photoURL: photoUrl,
       });
-    };
-    const signInUser = (email, password) => {
+   };
+   const signInUser = (email, password) => {
       setLoader(true);
       return signInWithEmailAndPassword(auth, email, password);
-    };
-    const signOutUser = () => {
+   };
+   const signOutUser = () => {
       return signOut(auth);
-    };
-  
-    const contextValue = {
+   };
+
+   const contextValue = {
       signUpUser,
       signInUser,
       signWithGoogle,
@@ -55,34 +56,52 @@ import app from "../Firebase/firebase.config";
       loader,
       signOutUser,
       updateUserProfile,
-    };
-    useEffect(() => {
+   };
+   useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-        setLoader(false);
-        // if (currentUser) {
-        //   const user = { email: currentUser.email };
-        //   axiosPublic.post("/user/accessToken", user).then((res) => {
-        //     const token = res.data.token;
-        //     if (token) {
-        //       localStorage.setItem("access-token", token);
-        //       setLoader(false);
-        //     }
-        //   });
-        // } else {
-        //   localStorage.removeItem("access-token");
-        //   setLoader(false);
-        // }
+         const userEmail = currentUser?.email || user?.email;
+         const loggedUser = { email: userEmail };
+
+         setUser(currentUser);
+         setLoader(false);
+         //  if (currentUser) {
+         //    const user = { email: currentUser.email };
+         //    axios.post("/user/accessToken", user).then((res) => {
+         //      const token = res.data.token;
+         //      if (token) {
+         //        localStorage.setItem("access-token", token);
+         //        setLoader(false);
+         //      }
+         //    });
+         //  } else {
+         //    localStorage.removeItem("access-token");
+         //    setLoader(false);
+         //  }
+         if (currentUser) {
+            axios
+               .post("http://localhost:5000/jwt", loggedUser, {
+                  withCredentials: true,
+               })
+               .then((res) => {
+                  console.log(res.data);
+               });
+         } else {
+            axios.post("http://localhost:5000/signout", loggedUser, {
+               withCredentials: true,
+            })
+            .then(res=>{
+              console.log(res.data)
+            })
+         }
       });
       return () => unsubscribe();
-    }, [auth]);
-    console.log(user);
-    return <Context.Provider value={contextValue}>{children}</Context.Provider>;
-  };
-  
-  export default AuthProvider;
-  
-  AuthProvider.propTypes = {
-    children: PropTypes.node,
-  };
-  
+   }, [auth]);
+   console.log(user);
+   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+   children: PropTypes.node,
+};
--- src/Pages/MyBookings/BookingCard.jsx
+++ src/Pages/MyBookings/BookingCard.jsx
@@ -1,63 +1,81 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 
-const BookingCard = ({ room,handleDelete }) => {
-    const [buttonDisable,setButtonDisabled] = useState(false);
-  const DeadlineDate = new Date(room?.bookingDate);
-  const today = new Date(Date.now());
-  const dayDifferent = Math.floor(
-    (DeadlineDate - today) / (1000 * 60 * 60 * 24)
-  );
+const BookingCard = ({ room }) => {
+   const [buttonDisable, setButtonDisabled] = useState(false);
+   const DeadlineDate = new Date(room?.bookingDate);
+   const today = new Date(Date.now());
+   const dayDifferent = Math.floor(
+      (DeadlineDate - today) / (1000 * 60 * 60 * 24)
+   );
 
-  useEffect(() => {
-    if (dayDifferent < 1) {
-      setButtonDisabled(true);
-    }
-  }, [dayDifferent]);
-  console.log(buttonDisable);
-  return (
-    <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-      <img
-        className="object-cover w-full h-64"
-        src={room.room_image}
-        alt="Article"
-      />
+   const handleDelete = (_id) => {
+      fetch(`http://localhost:5001/users/${_id}`, {
+         method: "DELETE",
+      })
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+         });
+   };
+   useEffect(() => {
+      if (dayDifferent < 1) {
+         setButtonDisabled(true);
+      }
+   }, [dayDifferent]);
+   console.log(buttonDisable);
+   return (
+      <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+         <img
+            className="object-cover w-full h-64"
+            src={room.room_image}
+            alt="Article"
+         />
 
-      <div className="p-6">
-        <div>
-          <a
-            href="#"
-            className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
-            tabIndex="0"
-            role="link"
-          >
-            {room.room_name}
-          </a>
-          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-            {room.description}
-          </p>
-        </div>
-
-        <div className="mt-4">
-          <div className="flex items-center justify-between">
-            <h1>
-              Booked At :{" "}
-              <span className="mx-1  text-gray-600 dark:text-gray-300">
-                {room.bookingDate}
-              </span>
-            </h1>
-            <div className="flex gap-2">
-              <button onClick={()=>handleDelete(room._id)} disabled={buttonDisable} className={`${buttonDisable ? "py-1 px-4 rounded-sm bg-gray-300  text-white font-semibold !cursor-not-allowed":'py-1 px-4 rounded-sm bg-main  text-white font-semibold '} `}>
-                delete
-              </button>
-              <button className="py-1 px-4 rounded-sm bg-green-600  text-white font-semibold" >
-                update
-              </button>
+         <div className="p-6">
+            <div>
+               <a
+                  href="#"
+                  className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
+                  tabIndex="0"
+                  role="link"
+               >
+                  {room.room_name}
+               </a>
+               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                  {room.description}
+               </p>
             </div>
-          </div>
-        </div>
+
+            <div className="mt-4">
+               <div className="flex items-center justify-between">
+                  <h1>
+                     Booked At :{" "}
+                     <span className="mx-1  text-gray-600 dark:text-gray-300">
+                        {room.bookingDate}
+                     </span>
+                  </h1>
+                  <div className="flex gap-2">
+                     <button
+                        onClick={() => handleDelete(room._id)}
+                        disabled={buttonDisable}
+                        className={`${
+                           buttonDisable
+                              ? "py-1 px-4 rounded-sm bg-gray-300  text-white font-semibold !cursor-not-allowed"
+                              : "py-1 px-4 rounded-sm bg-main  text-white font-semibold "
+                        } `}
+                     >
+                        delete
+                     </button>
+                     <button className="py-1 px-4 rounded-sm bg-green-600  text-white font-semibold">
+                        update
+                     </button>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default BookingCard;
